cb6649ffaf37d607c0e384f363ae11c484283fdd
leongli <79017154+leongli@users.noreply.github.com>
2021-10-10 22:09:22 -0400

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,40 @@
+import java.awt.Color;
+
+public class Rectangle extends Shape{
+	public int width;
+	public int height;
+	public Rectangle(int x,int y,int width,int height,Color color) {
+		super (x,y,color);
+		this.height=height;
+		this.width=width;
+	}
+	public int getSurfaceArea()
+	{
+		return (width*height);
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public int compareTo(Shape o) {
+		if (this.getSurfaceArea()> o.getSurfaceArea())
+		{
+			return 1;
+		}else if (this.getSurfaceArea()< o.getSurfaceArea())
+		{
+			return -1;
+		}
+		else return 0;
+	}
+}
